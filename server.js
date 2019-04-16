d64f14c3088b1c604b663ea320259931738712e0
Orlando <orlando-junior@live.com>
2019-04-16 18:15:35 -0300

    commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const net = require('net');
+const port = 7070;
+const host = '127.0.0.1';
+
+const server = net.createServer();
+server.listen(port, host, () => {
+    console.log('Servidor TCP executando na porta ' + port + '.');
+});
+
+let sockets = [];
+
+server.on('connection', function(sock) {
+    console.log('CONECTADO: ' + sock.remoteAddress + ':' + sock.remotePort);
+    sockets.push(sock);
+
+    sock.on('data', function(data) {
+        console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        // Write the data back to all the connected, the client will receive it as data from the server
+        sockets.forEach(function(sock, index, array) {
+            sock.write(sock.remoteAddress + ':' + sock.remotePort + " disse " + data + '\n');
+        });
+    });
+
+    // Add a 'close' event handler to this instance of socket
+    sock.on('close', function(data) {
+        let index = sockets.findIndex(function(o) {
+            return o.remoteAddress === sock.remoteAddress && o.remotePort === sock.remotePort;
+        })
+        if (index !== -1) sockets.splice(index, 1);
+        console.log('FECHADA: ' + sock.remoteAddress + ' ' + sock.remotePort);
+    });
+});
