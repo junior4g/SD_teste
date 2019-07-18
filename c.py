54d31957f9e13b828623d5c0aa72764eba4d8c73
Orlando <orlando-junior@live.com>
2019-07-18 09:50:27 -0300

    commit

--- c.py
+++ c.py
@@ -4,9 +4,14 @@ import paho.mqtt.client as mqtt
 import time
 from datetime import datetime
 
-# -------- Configurações --------------	
-# Definicão do broker (server). Opções online: test.mosquitto.org; iot.eclipse.org ou outro qualquer
-broker_init = "test.mosquitto.org" #broker local -> mudar depois para o local certo (ip ou nome da máq. na aws)
+#Limpar tela
+import os
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+#broker local -> mudar depois para o local certo (ip ou nome da máq. na aws)
+#broker_init = "test.mosquitto.org"
+broker_init = "test.mosquitto.org"
 
 #broker = input("Informe o broker, sugestão: 'test.mosquitto.org' ou ENTER  para localhost: ")
 #if len(broker) < 1: broker = broker_init
@@ -17,9 +22,8 @@ nclientname = 'cliente'
 clientname = nclientname + '-' + str(datetime.now()) 
 
 # o datatime é só para não criar cliente com nomes iguais. "gamb" (arrumar uma forma mais elegante)
-
-tpc = str("projetosd:teste")
 #registrando o nome do topico --- aqui será dinâmico configurado pelo usuário
+tpc = str("projetosd:teste")
 
 # Falta tratar as excessões
 def on_log(client, userdata, level, buf):
@@ -35,9 +39,10 @@ def on_disconnet (client, userdata, flags, rc=0):
 	print("Desconectado, codigo"+str(rc))	
 
 def on_message(client,userdata,msg):
-	topic=msg.topic
-	m_decode=str(msg.payload.decode("utf-8","ignore"))
-	#print("["+str(msg.topic)+"]:",m_decode)
+	topic    = msg.topic
+	m_decode = str(msg.payload.decode("utf-8","ignore"))
+	
+	#print(str(msg.topic)+" : ",m_decode)
 
 
 # instanciando o cliente. 
@@ -61,12 +66,147 @@ client.loop_start()
 # Entrada de dados (acesso) --> modularizar depois, usar loop e sleep
 print("Aguardando entrada de dados ....")
 print("--------------------------------")
+
 cnt = 1
 
 while(cnt > 0):
-	#msg_acess = input("parada: ")
-	client.publish(tpc,str(input("Digite a mensagem: "))) 
- 
+		#msg_acess = input("parada: ")
+		#client.publish(tpc,str(input("Digite a mensagem: ")))
+
+		# tcp.send (msg.encode('utf-8'))
+		# msg = input()
+		cls()
+		print("\n\n")
+		print("                     CONTROLE DE ACESSO DE CONDOMINIO                          ")
+		print("                      GERENCIAMENTO - PAGINA INICIAL                           ")
+		print("\n")
+		print(" ##############################################################################")
+		print(" |                                                                            |")
+		print(" |   Digite um comando para prosseguir:                                       |")
+		print(" |   1 - Cadastrar politica de acesso                                         |")
+		print(" |   2 - Cadastrar usuarios                                                   |")
+		print(" |   3 - Cadastrar acesso                                                     |")
+		print(" |   4 - Cadastrar predios                                                    |")
+		print(" |   5 - Teste de envio de mensagem                                           |")
+		print(" |                                                                            |")
+		print(" ##############################################################################")
+		men = input("  Digite a opcao escolhida: ")
+
+		men_int = int(men)  # Convertendo a mensagem em um inteiro
+
+		if men_int == 1:
+			cls()
+			print("\n\n")
+			print("                        CADASTRAR POLITICA DE ACESSO                           ")
+			print("\n")
+			print(" ##############################################################################")
+			print(" |                                                                            |")
+			print(" |   Voce deve informar a que usuario a politica de acesso se destina.        |")
+			print(" |   - Funcionario                                                            |")
+			print(" |   - Visitante                                                              |")
+			print(" |   - Prestador de servico                                                   |")
+			print(" |                                                                            |")
+			print(" ##############################################################################")
+			id = input(" ID: ")
+			politica = input(" Tipo de usuario: ")
+			local_de_acesso = input(" Local de acesso: ")
+			horario_inicio = input(" Horario de inicio: ")
+			horario_fim = input(" Horario final: ")
+
+			men = "1#" + id + "#" + politica + "#" + local_de_acesso + "#" + horario_inicio + "#" + horario_fim
+
+			client.publish(tpc, str(men))
+			#tcp.send(men.encode('utf-8'))
+			# sock.send(bytes(str(men), "utf-8"))  # Envia mensagem para o servidor
+
+		elif men_int == 2:
+
+			cls()
+			print("\n\n")
+			print("                              CADASTRAR USUARIOS                               ")
+			print("\n")
+			print(" ##############################################################################")
+			print(" |                                                                            |")
+			print(" |   Tipos de usuario                                                         |")
+			print(" |   - Funcionario                                                            |")
+			print(" |   - Visitante                                                              |")
+			print(" |   - Prestador de servico                                                   |")
+			print(" |                                                                            |")
+			print(" ##############################################################################")
+			id = input(" ID: ")
+			matricula = input(" Matricula: ")
+			tipo_de_acesso = input(" Tipo de acesso: ")
+
+			men = "2#" + id + "#" + matricula + "#" + tipo_de_acesso
+
+			client.publish(tpc, str(men))
+			#tcp.send(men.encode('utf-8'))
+			# sock.send(bytes(str(men), "utf-8"))  # Envia mensagem para o servidor
+
+		elif men_int == 3:
+
+			cls()
+			print("\n\n")
+			print("                              CADASTRAR ACESSO                                 ")
+			print("\n")
+			print(" ##############################################################################")
+			print(" |                                                                            |")
+			print(" |   Cadastrar acesso dos usuarios                                            |")
+			print(" |                                                                            |")
+			print(" |   Opcoes:                                                                  |")
+			print(" |                                                                            |")
+			print(" |   Predio 01                                                                |")
+			print(" |   - 12 andares                                                             |")
+			print(" |                                                                            |")
+			print(" |   Predio 02                                                                |")
+			print(" |   - 12 andares                                                             |")
+			print(" |                                                                            |")
+			print(" ##############################################################################")
+			matricula = input(" Matricula: ")
+			predio = input(" Predio: ")
+			andar = input(" Andar: ")
+			tipo_de_acesso = input(" Tipo de acesso: ")
+
+			men = "3#" + matricula + "#" + predio + "#" + andar + "#" + tipo_de_acesso
+
+			#operacao, matricula, predio, andar, tipo_de_acesso
+
+			client.publish(tpc, str(men))
+			#tcp.send(men.encode('utf-8'))
+			# sock.send(bytes(str(men), "utf-8"))  # Envia mensagem para o servidor
+
+		elif men_int == 4:
+
+			cls()
+			print("\n\n")
+			print("                              CADASTRAR PREDIOS                                ")
+			print("\n")
+			print(" ##############################################################################")
+			print(" |                                                                            |")
+			print(" |   Cadastrar predios no condominio                                          |")
+			print(" |                                                                            |")
+			print(" |   Informe os dados solicitados                                             |")
+			print(" |                                                                            |")
+			print(" ##############################################################################")
+			id = input(" ID: ")
+			nome = input(" Nome: ")
+			numero_de_andares = input(" Numero de andares: ")
+			capacidade_de_pessoas_por_andar = input(" Capacidade de pessoas por andar: ")
+
+			men = "4#" + id + "#" + nome + "#" + numero_de_andares + "#" + capacidade_de_pessoas_por_andar
+
+			client.publish(tpc, str(men))
+			#tcp.send(men.encode('utf-8'))
+			# sock.send(bytes(str(men), "utf-8"))  # Envia mensagem para o servidor
+
+		elif men_int == 5:
+			client.publish(tpc, str("5#123#P01#CAT00#A03"))
+			#tcp.send(men.encode('utf-8'))
+			# sock.send(bytes(str(men_int), "utf-8"))  # Envia mensagem para o servidor
+
+		else:
+			print("Nao existe esta opcao, tente novamente!", "\n")
+
 
 # Tempo conexão ...
 #para loop
--- s.py
+++ s.py
@@ -1,21 +1,30 @@
-
 #Packs
 import sys
 import paho.mqtt.client as mqtt
 import time
 from datetime import datetime
 
-# -------- Configurações --------------	
+import redis
+conn = redis.Redis('localhost')
+
+import os
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+cls()
+
+data_e_hora_atuais = datetime.now()
+data_e_hora_str = data_e_hora_atuais.strftime('%H:%M:%S %d/%m/%Y')
 
 # Definicão do broker (server). Opções online: test.mosquitto.org; iot.eclipse.org ou outro qualquer
-broker_init = "test.mosquitto.org" #broker local -> mudar depois para o local certo (ip ou nome da máq. na aws)
+# broker local -> mudar depois para o local certo (ip ou nome da máq. na aws)
+# test.mosquitto.org
+broker_init = "test.mosquitto.org"
 
-
-#broker = input("Informe o broker, sugestão: 'test.mosquitto.org' ou ENTER  para localhost: ")
-#if len(broker) < 1: broker = broker_init
+# broker = input("Informe o broker, sugestão: 'test.mosquitto.org' ou ENTER  para localhost: ")
 broker = broker_init
 
-#nclientname = input("Informe o número da catraca/porta de acesso: ")
+# nclientname = input("Informe o número da catraca/porta de acesso: ")
 nclientname = 'cliente'
 clientname = nclientname + '-' + str(datetime.now()) 
 
@@ -23,7 +32,6 @@ clientname = nclientname + '-' + str(datetime.now())
 tpc_base = "projetosd:teste"
 tpc = str(tpc_base)
 #tpc = str(tpc_base + "/" + str(nclientname))
-#registrando o nome do topico --- aqui será dinâmico configurado pelo usuário
 
 # Falta tratar as excessões
 def on_log(client, userdata, level, buf):
@@ -40,19 +48,72 @@ def on_disconnet (client, userdata, flags, rc=0):
 	print("Desconectado, codigo"+str(rc))	
 
 def on_message(client,userdata,msg):
-	topic=msg.topic
-	m_decode=str(msg.payload.decode("utf-8","ignore"))
-	print("Mensagem recebida: ",m_decode)
-	print("Cliente: ", client)
-	print("Userdata: ", userdata)
-	print("---------------------------------------------------------")
+
+
+	topic    = msg.topic
+	m_decode = str(msg.payload.decode("utf-8","ignore"))
 	
-	#split_msg = m_decode.split('#') #separa campos
-	#print(str(msg.topic) + "#", m_decode)
-	#print(split_msg[0]) #origem (tópico)
-	#print(split_msg[1]) #codigo do funcionario/ visitante
-	#print(split_msg[2]) #data-hora de acesso
-	#print("------")	
+	print("Mensagem recebida: ",m_decode)
+	print("Cliente: ",client)
+	print("Topico: ", topic)
+
+	data_e_hora_atuais = datetime.now()
+	data_e_hora_str = data_e_hora_atuais.strftime('%H:%M:%S %d/%m/%Y')
+	print("Horario: ", data_e_hora_str)
+
+	##################### Inicio operações com Redis #######################
+	operacao = int(m_decode[0])
+
+	if operacao == 1:
+		# Opção cadastrar politica de acesso
+
+		operacao, id, politica, local_de_acesso, horario_inicio, horario_fim = m_decode.split('#')
+
+		politica = {"id": id, "politica": politica, "local_de_acesso": local_de_acesso,
+					"horario_de_inicio": horario_inicio, "horario_fim": horario_fim}
+		conn.hmset("politica:" + id, politica)
+
+		print("Comando para consulta no Redis: hgetall politica:" + id)
+
+	elif operacao == 2:
+		# Opção cadastrar usuarios
+
+		operacao, id, matricula, tipo_de_acesso = m_decode.split('#')
+
+		user = {"id": id, "matricula": matricula, "tipo_de_acesso": tipo_de_acesso}
+		conn.hmset("user:" + id, user)
+
+		print("Comando para consulta no Redis: hgetall user:" + id)
+
+	elif operacao == 3:
+		# Opção cadastrar acesso
+
+		operacao, matricula, predio, andar, tipo_de_acesso = m_decode.split('#')
+
+		acesso = {"matricula": matricula, "predio": predio, "andar": andar, "tipo_de_acesso": tipo_de_acesso}
+		conn.hmset("acesso:" + matricula, acesso)
+
+		print("Comando para consulta no Redis: hgetall acesso:" + matricula)
+
+		#print("\n Operacao 3")
+
+	elif operacao == 4:
+		# Opção cadastrar prédios
+
+		operacao, id, nome, numero_de_andares, capacidade_de_pessoas_por_andar = m_decode.split('#')
+
+		predio = {"id": id, "nome": nome, "numero_de_andares": numero_de_andares,
+				  "capacidade_de_pessoas_por_andar": capacidade_de_pessoas_por_andar}
+		conn.hmset("predio:" + id, predio)
+
+		print("Comando para consulta no Redis: hgetall predio:" + id)
+
+	elif operacao == 5:
+		print("Operacao 5 sendo foi executada")
+
+	print("-------------------------------------------------------------------------------")
+
+	##################### Final operações com Redis #######################
 
 # instanciando o cliente. 
 client = mqtt.Client (clientname) 
@@ -60,9 +121,10 @@ client.on_connect = on_connect
 client.on_disconnet = on_disconnet
 client.on_message = on_message
 print("Conectado no broker", broker)
+print("Horario: ", data_e_hora_str)
 
-print("\n                  RECEBENDO MENSAGENS                    ")
-print("---------------------------------------------------------")
+print("\n                             RECEBENDO MENSAGENS                             ")
+print("-------------------------------------------------------------------------------")
 
 # conectar ao broker
 client.connect(broker)
@@ -74,8 +136,8 @@ client.subscribe(str(tpc))
 client.loop_start()	
 
 # Tempo conexão ...
-#parar loop
+# parar loop
 time.sleep(1000) 
-#client.loop_stop()
-#desconetar cliente
-#client.disconnect()
+# client.loop_stop()
+# desconetar cliente
+# client.disconnect()
